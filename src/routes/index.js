0d1376cec321c3a37c4e910f6c018f6cd9a048b2
PhuTam54 <phutamytb@gmail.com>
2024-01-04 14:34:58 +0700

    Add enjs practice

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,7 @@
-const blogRouter = require('./blog');
-const productRouter = require('./product');
-const userRouter = require('./user');
-const orderRouter = require('./order');
+const songRouter = require('./song');
 
 function route(app) {
-    app.use('/api/blogs', blogRouter);
-    app.use('/api/products', productRouter);
-    app.use('/api/users', userRouter);
-    app.use('/api/orders', orderRouter);
+    app.use('/api/songs', songRouter);
 }
 
 module.exports = route;
